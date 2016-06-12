734d5d10a52610952e1de6164b5c1fcca7a36367
Nikolay Vasilev <vassilev.nikolay@gmail.com>
2016-06-12 17:14:46 +0300

    vähän lomituslajittelua

--- src/parallelmergesort.java
+++ src/parallelmergesort.java
@@ -1,5 +1,23 @@
-/**
- * Created by Nikolay Vasilev on 12.06.2016.
- */
+
 public class parallelmergesort {
+
+    public static void main(String[] args){
+        final int taulukkoPituus = 10000;
+        int[] taulukko = new int[taulukkoPituus];
+        mergesort(taulukko,0,taulukkoPituus-1);
+    }
+
+    private static void mergesort(int[] taulukko, int ala, int yla) {
+        if(ala < yla){
+            int mid = (ala+yla)/2;
+            mergesort(taulukko,ala,mid);
+            mergesort(taulukko,mid+1,yla);
+            merge(taulukko,ala,mid,yla);
+        }
+    }
+
+    private static void merge(int[] taulukko, int ala, int mid, int yla) {
+        int min = Math.min(mid-ala+1,yla-mid);
+
+    }
 }
